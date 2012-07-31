640e35c4e50a111bd27e05a08d4377cf165b46e2
Oliver Yasuna <oliver-fc@gmail.com>
2012-07-31 19:31:34 -0400

    Recoded PluginHandler. See description.
    Now you dont need *.config in the plugins folder. They will be in the
    jar. The .config will still define the main class, but now like:
    main-class = (main class)

--- src/com/gamezgalaxy/GGS/API/plugin/PluginHandler.java
+++ src/com/gamezgalaxy/GGS/API/plugin/PluginHandler.java
@@ -8,13 +8,18 @@
 package com.gamezgalaxy.GGS.API.plugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.Scanner;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import com.gamezgalaxy.GGS.server.Server;
 
@@ -22,8 +27,117 @@ public class PluginHandler {
 	private ArrayList<Plugin> plugins = new ArrayList<Plugin>();
 	
 	private ArrayList<Game> games = new ArrayList<Game>();
+
+	public void loadplugins(Server server)
+	{
+		// TODO: NOT TESTED! Waiting for Eddie to make the server stable with his packet stuff.
+		// TODO: Clean up a bit. Use less memory.
+
+		// Basically instead of having for example "All-In-One.config" in plugins/, you put it inside the jar All-In-One.config.
+		// The first line should be "main-class = org.whatever.hello.Main" (your main class).
+
+		File pluginFolder = new File("plugins/");
+
+		if(!pluginFolder.exists())
+		{
+			System.out.println("Mods folder does not exist.");
+
+			return;
+		}
+
+		File[] pluginFiles = pluginFolder.listFiles();
+
+		for(int i = 0; i < pluginFiles.length; i++)
+		{
+			if(pluginFiles[i].isFile() && pluginFiles[i].getName().endsWith(".jar"))
+			{
+				JarFile file = null;
+
+				try {
+					file = new JarFile(pluginFiles[i]);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				if(file != null)
+				{
+					Enumeration<JarEntry> entries = file.entries();
+
+					if(entries != null)
+					{
+						while(entries.hasMoreElements())
+						{
+							JarEntry fileName = entries.nextElement();
+
+							if(fileName.getName().endsWith(".config"))
+							{
+								Properties properties = new Properties();
+
+								try {
+									properties.load(file.getInputStream(fileName));
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+
+								File pluginFile = new File("plugins/" + pluginFiles[i].getName());
+								String[] args = new String[] { "-normal" };
+
+								loadplugin(pluginFile, properties.getProperty("main-class"), args, server);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private String getClass(File file)
+	{
+		try {
+			Scanner scan = new Scanner(file);
+
+			return scan.nextLine();
+		} catch(Exception e) {
+			return "null";
+		}
+	}
+
+	public void loadplugin(File file, String classpath, String[] args, Server server)
+	{
+		// TODO: NOT TESTED! Waiting for Eddie to make the server stable with his packet stuff.
+
+		try {
+			URL[] urls = new URL[] { file.toURL() };
+			ClassLoader loader = URLClassLoader.newInstance(urls, getClass().getClassLoader());
+			Class<?> class_ = Class.forName(classpath, true, loader);
+			Class<? extends Plugin> runClass = class_.asSubclass(Plugin.class);
+			Constructor<? extends Plugin> constructor = runClass.getConstructor(Server.class);
+			Plugin plugin = constructor.newInstance();
+
+			plugin.onLoad(args);
+
+			if(plugin instanceof Game)
+			{
+				games.add((Game)plugin);
+			} else {
+				plugins.add(plugin);
+			}
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	public void loadplugins(Server server) {
+	/*public void loadplugins(Server server) {
 		if (!new File("plugins/").exists())
 			new File("plugins/").mkdir();
 		File folder = new File("plugins/");
@@ -87,6 +201,6 @@ public class PluginHandler {
 			games.add((Game)doRun);
 		else
 			plugins.add(doRun);
-	}
+	}*/
 
 }
