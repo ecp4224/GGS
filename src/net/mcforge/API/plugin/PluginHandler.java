79886e656a58ac6f97c43dacdb547232be8539b8
ninedrafted <none@example.org>
2012-09-30 00:02:58 +0200

    loading plugins by .class file
    if a .config file is present under the same location and name it gets
    passed to to the constructer or to setProperties if there is no constructer
    accepting the properties as parameter

--- src/net/mcforge/API/plugin/PluginHandler.java
+++ src/net/mcforge/API/plugin/PluginHandler.java
@@ -7,15 +7,9 @@
  ******************************************************************************/
 package net.mcforge.API.plugin;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
@@ -42,139 +36,6 @@ public class PluginHandler {
 			plugins.remove(p);
 		}
 	}
-	private File[] checkrequires(Server server)
-	{
-		ArrayList<File> correctfiles = new ArrayList<File>();
-		ArrayList<String> pluginnames = new ArrayList<String>();
-		File pluginFolder = new File("plugins/");
-		File[] pluginFiles = pluginFolder.listFiles();
-		for(int i = 0; i < pluginFiles.length; i++)
-		{
-			if(pluginFiles[i].isFile() && pluginFiles[i].getName().endsWith(".jar"))
-			{
-				JarFile file = null;
-				try {
-					file = new JarFile(pluginFiles[i]);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				if(file != null)
-				{
-					Enumeration<JarEntry> entries = file.entries();
-					if(entries != null)
-					{
-						while(entries.hasMoreElements())
-						{
-							JarEntry fileName = entries.nextElement();
-							if(fileName.getName().endsWith(".config"))
-							{
-								Properties properties = new Properties();
-								try {
-									properties.load(file.getInputStream(fileName));
-								} catch (IOException e) {
-									e.printStackTrace();
-								}
-								new File("plugins/" + pluginFiles[i].getName());
-								DataInputStream in = null;
-								try {
-									in = new DataInputStream(file.getInputStream(fileName));
-								} catch (IOException e2) {
-									e2.printStackTrace();
-								}
-								BufferedReader br = new BufferedReader(new InputStreamReader(in));
-								String strLine;
-								try {
-									while ((strLine = br.readLine()) != null) {
-										String key = strLine.split("\\=")[0].trim();
-										String value = strLine.split("\\=")[1].trim();
-										if (key.equals("name")) {
-											pluginnames.add(value.trim().toLowerCase());
-										}
-									}
-								} catch (IOException e1) {
-									e1.printStackTrace();
-								}
-							}
-						}
-					}
-					try {
-						file.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-		for(int i = 0; i < pluginFiles.length; i++)
-		{
-			if(pluginFiles[i].isFile() && pluginFiles[i].getName().endsWith(".jar"))
-			{
-				JarFile file = null;
-				try {
-					file = new JarFile(pluginFiles[i]);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				if(file != null)
-				{
-					Enumeration<JarEntry> entries = file.entries();
-					if(entries != null)
-					{
-						while(entries.hasMoreElements())
-						{
-							JarEntry fileName = entries.nextElement();
-							if(fileName.getName().endsWith(".config"))
-							{
-								Properties properties = new Properties();
-								try {
-									properties.load(file.getInputStream(fileName));
-								} catch (IOException e) {
-									e.printStackTrace();
-								}
-								new File("plugins/" + pluginFiles[i].getName());
-								DataInputStream in = null;
-								try {
-									in = new DataInputStream(file.getInputStream(fileName));
-								} catch (IOException e2) {
-									e2.printStackTrace();
-								}
-								BufferedReader br = new BufferedReader(new InputStreamReader(in));
-								String strLine;
-								try {
-									boolean canadd = true;
-									while ((strLine = br.readLine()) != null) {
-										String key = strLine.split("\\=")[0].trim();
-										String value = strLine.split("\\=")[1].trim();
-										if (key.equals("required-plugins")) {
-											String[] required = value.split(",");
-											for (int z=0;z<required.length;z++)
-											{
-												if (!pluginnames.contains(required[z].trim().toLowerCase()))
-													canadd = false;
-											}
-										}
-										else
-										{
-										    correctfiles.add(pluginFiles[i]);
-										}
-									}
-									if (canadd) { correctfiles.add(pluginFiles[i]); }
-								} catch (IOException e1) {
-									e1.printStackTrace();
-								}
-							}
-						}
-					}
-					try {
-						file.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-		return correctfiles.toArray(new File[correctfiles.size()]);
-	}
 
 	public void loadPlugin(Server server, File arg0) {
 		JarFile file = null;
@@ -183,152 +44,122 @@ public class PluginHandler {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if(file != null)
-		{
+		if (file != null) {
 			Enumeration<JarEntry> entries = file.entries();
-			if(entries != null)
-			{
-				while(entries.hasMoreElements())
-				{
+
+			if (entries != null) {
+				while (entries.hasMoreElements()) {
 					JarEntry fileName = entries.nextElement();
-					if(fileName.getName().endsWith(".config"))
-					{
-						Properties properties = new Properties();
+					if (fileName.getName().endsWith(".class")) {
 						try {
-							properties.load(file.getInputStream(fileName));
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-						File pluginFile = new File("plugins/" + arg0.getName());
-						String[] args = new String[] { "-normal" };
-						DataInputStream in = null;
-						try {
-							in = new DataInputStream(file.getInputStream(fileName));
-						} catch (IOException e2) {
-							e2.printStackTrace();
-						}
-						BufferedReader br = new BufferedReader(new InputStreamReader(in));
-						String strLine;
-						try {
-							while ((strLine = br.readLine()) != null) {
-								String key = strLine.split("\\=")[0].trim();
-								String value = strLine.split("\\=")[1].trim();
-								if (key.equals("main-class")) {
-									Plugin plugin = getType(pluginFile, value, Plugin.class, server);
-									for (Plugin p : plugins) {
-										if (p.equals(plugin))
-											continue;
-									}
-									plugin.setProperties(properties);
-									plugin.onLoad(args);
-									if(plugin instanceof Game)
-									{
-										games.add((Game)plugin);
-										server.Add((Game)plugin);
-									} else {
-										plugins.add(plugin);
+							String fullName = fileName.getName();
+							int lastSlash = fullName.lastIndexOf('/');
+							String path = fullName.substring(0, lastSlash + 1);
+							System.out.println(fullName.length() + "-" + path.length() + "-" + ".class".length());
+							String name = fullName.substring(path.length(), fullName.length() - ".class".length());
+
+							URL[] urls = new URL[]{arg0.toURI().toURL()};
+							ClassLoader loader = URLClassLoader.newInstance(urls, getClass().getClassLoader());
+							Class<?> class_ = Class.forName(path.replace('/', '.') + name, true, loader);
+							if (Plugin.class.isAssignableFrom(class_)) {
+								Class<? extends Plugin> pluginClass = class_.asSubclass(Plugin.class);
+								Constructor<? extends Plugin> constructByServer = null;
+								Exception tmpEx = null;
+								try {
+									constructByServer = pluginClass.getConstructor(Server.class);
+								} catch (Exception ex) {
+									tmpEx = ex;
+								}
+								Constructor<? extends Plugin> constructByServerProperties = null;
+								try {
+									constructByServerProperties = pluginClass.getConstructor(Server.class, Properties.class);
+								} catch (Exception ex) {
+									if (tmpEx != null) {
+										tmpEx.printStackTrace();
+										ex.printStackTrace();
 									}
 								}
-								if (key.equals("command-path")) {
-									Command c = getType(pluginFile, value, Command.class);
+
+								JarEntry propFile = file.getJarEntry(path + name + ".config");
+								Properties properties = new Properties();
+								if (propFile != null) {
+									try {
+										properties.load(file.getInputStream(propFile));
+									} catch (Exception ex) {
+										ex.printStackTrace();
+									}
+								}
+
+								Plugin plugin = null;
+								if (constructByServerProperties != null) {
+									try {
+										plugin = constructByServerProperties.newInstance(server, properties);
+									} catch (Exception ex) {
+										ex.printStackTrace();
+									}
+								}
+								if (constructByServer != null && plugin == null) {
+									try {
+										plugin = constructByServer.newInstance(server);
+									} catch (Exception ex) {
+										ex.printStackTrace();
+									}
+									if(plugin != null) plugin.setProperties(properties);
+								}
+								if (plugin == null) {
+									System.out.println("Plugin could not be load: " + name);
+									continue;
+								}
+
+								plugin.onLoad(new String[]{"-normal"});
+								if (plugin instanceof Game) {
+									games.add((Game) plugin);
+									server.Add((Game) plugin);
+								} else {
+									plugins.add(plugin);
+								}
+							} else {
+								if (!Command.class.isAssignableFrom(class_)) {
+									continue;
+								}
+								try {
+									Class<? extends Command> commandClass = class_.asSubclass(Command.class);
+									Constructor<? extends Command> construct = commandClass.getConstructor();
+									Command c = construct.newInstance();
 									server.getCommandHandler().addCommand(c);
+								} catch (Exception ex) {
+									ex.printStackTrace();
 								}
 							}
-						} catch (IOException e1) {
-							e1.printStackTrace();
-						}
-						try {
-							addPath(pluginFile);
-						} catch (MalformedURLException e) {
-							e.printStackTrace();
-						} catch (NoSuchMethodException e) {
-							e.printStackTrace();
-						} catch (SecurityException e) {
-							e.printStackTrace();
-						} catch (IllegalAccessException e) {
-							e.printStackTrace();
-						} catch (IllegalArgumentException e) {
-							e.printStackTrace();
-						} catch (InvocationTargetException e) {
-							e.printStackTrace();
-						}
-						try {
-							in.close();
-						} catch (IOException e) {
-							e.printStackTrace();
+
+						} catch (ClassNotFoundException ex) {
+							ex.printStackTrace();
+						} catch (IOException ex) {
+							ex.printStackTrace();
 						}
 					}
 				}
-			}
-			try {
-				file.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+				try {
+					file.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
-	public void loadplugins(Server server)
-	{
+
+	public void loadplugins(Server server) {
 		File pluginFolder = new File("plugins/");
-		if(!pluginFolder.exists())
-		{
+		if (!pluginFolder.exists()) {
 			System.out.println("Mods folder does not exist.");
 			pluginFolder.mkdir();
 			return;
 		}
-		File[] pluginFiles = checkrequires(server);
-		for(int i = 0; i < pluginFiles.length; i++)
-		{
-			if(pluginFiles[i].isFile() && pluginFiles[i].getName().endsWith(".jar"))
+		File[] pluginFiles = pluginFolder.listFiles();
+		for (int i = 0; i < pluginFiles.length; i++) {
+			if (pluginFiles[i].isFile() && pluginFiles[i].getName().endsWith(".jar")) {
 				loadPlugin(server, pluginFiles[i]);
-		}
-	}
-
-	@SuppressWarnings("deprecation")
-	public <T> T getType(File file, String classpath, Class<? extends T> type, Object...parma) {
-		try {
-			URL[] urls = new URL[] { file.toURL() };
-			ClassLoader loader = URLClassLoader.newInstance(urls, getClass().getClassLoader());
-			Class<?> class_ = Class.forName(classpath, true, loader);
-			Class<? extends T> runclass = class_.asSubclass(type);
-			Class<?>[] constructparma = new Class<?>[parma.length];
-			for (int i = 0; i < parma.length; i++) {
-				constructparma[i] = parma[i].getClass();
 			}
-			Constructor<? extends T> construct = runclass.getConstructor(constructparma);
-			T toreturn = construct.newInstance(parma);
-			return toreturn;
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-		return null;
-	}
-
-	/**
-	 * Adds a jar file to the System Classpath at runtime
-	 * @param f
-	 *         The file to add
-	 * @throws MalformedURLException
-	 *                              If the file path is not in the correct format
-	 * @throws NoSuchMethodException
-	 *                              If there was a problem adding the jar to the classpath
-	 * @throws SecurityException
-	 *                           If there was a problem adding the jar to the classpath
-	 * @throws IllegalAccessException
-	 *                                If there was a problem adding the jar to the classpath
-	 * @throws IllegalArgumentException
-	 *                                  If there was a problem adding the jar to the classpath
-	 * @throws InvocationTargetException
-	 *                                    If there was a problem adding the jar to the classpath
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked", "deprecation" })
-	public void addPath(File f) throws MalformedURLException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		URL u = f.toURL();
-		URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-		Class urlClass = URLClassLoader.class;
-		Method method = urlClass.getDeclaredMethod("addURL", new Class[]{URL.class});
-		method.setAccessible(true);
-		method.invoke(urlClassLoader, new Object[]{u});
-
 	}
 }
