0bfa2965bd593ad0d83182d013ff2a3bf8ded9f9
hypereddie10 <eddie@gamezgalaxy.com>
2012-10-07 19:02:32 -0400

    Added ServerStartedEvent

---
+++ src/net/mcforge/API/server/ServerEvent.java
@@ -0,0 +1,30 @@
+package net.mcforge.API.server;
+
+import net.mcforge.API.Event;
+import net.mcforge.API.EventList;
+import net.mcforge.server.Server;
+
+public abstract class ServerEvent extends Event {
+	
+	private Server server;
+	
+	public ServerEvent(Server server) {
+		this.server = server;
+	}
+	
+	/**
+	 * Get the server this event took place in
+	 * @return
+	 *        The {@link Server} object
+	 */
+	public Server getServer() {
+		return server;
+	}
+	
+	/**
+	 * Get a list of registered listeners
+	 * @return The list of listeners
+	 */
+	public abstract EventList getEvents();
+
+}
---
+++ src/net/mcforge/API/server/ServerStartedEvent.java
@@ -0,0 +1,26 @@
+package net.mcforge.API.server;
+
+import net.mcforge.API.EventList;
+import net.mcforge.server.Server;
+
+public class ServerStartedEvent extends ServerEvent {
+
+	private static EventList events = new EventList();
+	public ServerStartedEvent(Server server) {
+		super(server);
+	}
+
+	@Override
+	public EventList getEvents() {
+		return events;
+	}
+	
+	/**
+	 * Get a list of registered listeners
+	 * @return The list of listeners
+	 */
+	public static EventList getEventList() {
+		return events;
+	}
+
+}
