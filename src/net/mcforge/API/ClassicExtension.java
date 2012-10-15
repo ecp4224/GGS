37a1226a4317a18e9d613a582601fe1d946816af
hypereddie10 <eddie@gamezgalaxy.com>
2012-10-15 14:00:11 -0400

    Fixed ClassicExtension ElementType

--- src/net/mcforge/API/ClassicExtension.java
+++ src/net/mcforge/API/ClassicExtension.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 public @interface ClassicExtension {
 	String extName();
 	int version() default 1;
