a95a2898060e38d8a3152cf036d3aedd610941ff
hypereddie10 <eddie@gamezgalaxy.com>
2012-10-21 12:05:36 -0400

    Fixed verifynames

--- src/net/mcforge/iomodel/Player.java
+++ src/net/mcforge/iomodel/Player.java
@@ -320,7 +320,7 @@ public class Player extends IOClient implements CommandExecutor {
 	 * @return Returns true if the account is valid, otherwise it will return false
 	 */
 	public boolean VerifyLogin() {
-		return mppass.equals(getRealmppass()) && server.VerifyNames;
+		return server.VerifyNames ? mppass.equals(getRealmppass()) : true;
 	}
 	
 	/**
