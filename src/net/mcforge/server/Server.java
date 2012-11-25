6721e7c141fce2d5fa2340efc473e2c61ea4095b
Arrem <arrem.derplin@gmail.com>
2012-11-25 22:31:40 +0100

    Hai hipchat peeps! I miss ur! +Added getter and setter for physicsspeed in Level.java -Removed double license form Server.java

--- src/net/mcforge/server/Server.java
+++ src/net/mcforge/server/Server.java
@@ -5,14 +5,6 @@
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
  ******************************************************************************/
-/**
- * *****************************************************************************
- * Copyright (c) 2012 GamezGalaxy. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the GNU Public
- * License v3.0 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- *****************************************************************************
- */
 package net.mcforge.server;
 
 import java.io.File;
