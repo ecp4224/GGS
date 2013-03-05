6e2555dcfb01161de41047207ddc228d93bae875
Arrem <arrem.derplin@gmail.com>
2013-03-05 20:02:59 +0100

    More packets added +Added CloseWindow packet. +Added SpawnGlobalEntity packet. +Added UpdateWindowProperty packet.

--- src/net/mcforge/networking/packets/minecraft/SpawnGlobalEntity.java
+++ src/net/mcforge/networking/packets/minecraft/SpawnGlobalEntity.java
@@ -8,14 +8,14 @@ import net.mcforge.iomodel.SMPPlayer;
 import net.mcforge.networking.packets.PacketManager;
 import net.mcforge.server.Server;
 
-public class NamedSoundEffect extends SMPPacket {
+public class SpawnGlobalEntity extends SMPPacket {
 
-    public NamedSoundEffect(String name, byte ID, PacketManager parent) {
+    public SpawnGlobalEntity(String name, byte ID, PacketManager parent) {
         super(name, ID, parent);
     }
     
-    public NamedSoundEffect(PacketManager pm) {
-        this("NamedSoundEffect", (byte)0x3E, pm);
+    public SpawnGlobalEntity(PacketManager pm) {
+        this("SpawnGlobalEntity", (byte)0x47, pm);
     }
 
     @Override
@@ -24,20 +24,20 @@ public class NamedSoundEffect extends SMPPacket {
 
     @Override
     public void write(SMPPlayer player, Server server, Object... obj) {
-        if (obj.length >= 6) {
+        if (obj.length >= 5) {
             ByteBuffer bb;
             
-            if (obj[0] instanceof String && obj[1] instanceof Integer && obj[2] instanceof Integer &&
-                obj[3] instanceof Integer && obj[4] instanceof Float && obj[5] instanceof Byte) {
-                bb = ByteBuffer.allocate(20 + ((String)obj[1]).length() * 2);
+            if (obj[0] instanceof Integer && obj[1] instanceof Byte && obj[2] instanceof Integer &&
+                obj[3] instanceof Integer && obj[4] instanceof Integer) {
+                bb = ByteBuffer.allocate(18);
                 
                 bb.put(ID);
-                putMinecraftString((String)obj[0], bb);
-                bb.putInt((Integer)obj[1]);
+                bb.putInt((Integer)obj[0]);
+                bb.put((Byte)obj[1]);
                 bb.putInt((Integer)obj[2]);
                 bb.putInt((Integer)obj[3]);
-                bb.putFloat((Float)obj[4]);
-                bb.put((Byte)obj[5]);
+                bb.putInt((Integer)obj[4]);
+                
 				try {
 					player.writeData(bb.array());
 				}
