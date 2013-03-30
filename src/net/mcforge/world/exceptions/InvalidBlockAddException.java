c448b99598906dfcf198451540f9223b9a97c4c9
Eddie Penta <eddie@gamezgalaxy.com>
2013-03-30 13:31:28 -0400

    Mass copyright addition.

--- src/net/mcforge/world/exceptions/InvalidBlockAddException.java
+++ src/net/mcforge/world/exceptions/InvalidBlockAddException.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2013 MCForge.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
 package net.mcforge.world.exceptions;
 
 import net.mcforge.world.blocks.classicmodel.ClassicBlock;
